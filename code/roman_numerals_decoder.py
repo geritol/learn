f4f2d18177d33b1fe1206e38bacea335d6f9e4cf
geritol <gergo.tolnai@gmail.com>
2017-04-05 20:35:00 +0200

    simplified roman decoder

--- code/roman_numerals_decoder.py
+++ code/roman_numerals_decoder.py
@@ -1,7 +1,7 @@
 """
 Roman numerals decoder
 ---------------
-Input: string, containing a valid roman rumeral
+Input: string, containing a valid roman numeral
 Returns an integer (Arabic numeral representation of the Roman numeral)
 """
 
@@ -11,17 +11,14 @@ base_numbers = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
 def decode(roman):
     arabic = 0
-    for i in range(len(roman)):
-        current = roman[i]
-        current_value = base_numbers[current]
+    len_roman = len(roman)
+    for i in range(len_roman):
+        current_value = base_numbers[roman[i]]
 
-        # if the current item is smaller than the next one skip this iteration
-        if i + 1 != len(roman) and base_numbers[roman[i + 1]] > current_value:
+        # if the current item is smaller than the next one, subtract
+        if i + 1 != len_roman and base_numbers[roman[i + 1]] > current_value:
+            arabic -= current_value
             continue
-
-        # if we have a prefix, subtract
-        if i != 0 and base_numbers[roman[i - 1]] < current_value:
-            arabic -= base_numbers[roman[i - 1]]
 
         arabic += current_value
 
